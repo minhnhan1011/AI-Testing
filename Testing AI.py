1abaf60326c9ea8d1d1e5e5eab35f8a3e940f392
Nhân Liễu <lieuminhnhan1011@gmail.com>
2024-03-04 19:31:07 +0700

    update

--- Testing AI.py
+++ Testing AI.py
@@ -1,34 +1,34 @@
+# import file thu vien
 import cv2
 
-# loading the haar case algorithm file into alg variable
+# Lưu file thuat toán Haar Cascade Frontal Face để tham khao
 alg = "haarcascade_frontalface_default.xml"
 
 # passing the algorithm to OpenCV
 haar_cascade = cv2.CascadeClassifier(alg)
 
-# capturing the video feed from the camera
+# Dung camera de nhan dien khuon matc
 cam = cv2.VideoCapture(0)
 
 while True:
     _, img = cam.read()
     text = "Face not detected"
 
-# convert each frame from BGR to Grayscale
+# Đầu vào từ thế giới thực có nhiều màu sắc nhưng trong định dạng BGR. BGR là viết tắt của blue, green và red (xanh, xanh lá và đỏ). Điều này sẽ khiến ứng dụng thị giác máy phải xử lý rất nhiều thứ. Do vậy, để giảm bớt khối lượng quy trình, chúng ta sử dụng định dạng màu xám.
     grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# detect faces using Haar Cascade
     face = haar_cascade.detectMultiScale(grayImg, 1.3, 4)
 
-# draw a rectangle around the face and update the text to Face Detected
+# Khi bạn phát hiện ra khuôn mặt, bạn sẽ nhận được 4 tọa độ. Khi nhận diện đc khuôn mặt thì sẽ vẽ hình vuông với màu rgb
     for (x, y, w, h) in face:
         text = "Face Detected"
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-# display the text on the image
+# Hiện chữ treen ảnh
     print(text)
     image = cv2.putText(img, text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
-# display the output window and press escape key to exit
+# Hiển thị cửa sổ với tiêu đề Face Detection và hình ảnh. Sử dụng phương thức waitkey() để hiển thị cửa sổ trong 10 mili giây và kiểm tra một lần nhấn phím. Nếu người dùng nhấn phím Esc (Giá trị ASCII 27), hãy thoát khỏi vòng lặp.
     cv2.imshow("Face Detection", image)
     key = cv2.waitKey(10)
 
